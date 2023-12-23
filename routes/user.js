003f5167ff1eba0df96da6735b95de9d1ca71bcb
abmarrero <a.ernestomarrerol@gmail.com>
2023-12-22 21:42:29 -0500

    rolvalidate vs DB

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const {Router} = require('express');
 const { usersGet, usersPut, usersPost, usersDelete, usersPath } = require('../controllers/users.controller');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -15,6 +16,12 @@ const router = Router();
     check('password','El password debe de ser de más de 6 letras').isLength({min: 6}),
     check('rol','No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
     check('correo','El correo no es valido').isEmail(),
+    check('rol').custom(async(rol='')=>{
+      existeRol = await Role.findOne({rol});
+      if(!existeRol){
+        throw new Error(`el rol ${rol} no esta registrado en la BD`);
+      }
+    }),
       validarCampos
   ], usersPost)
 
