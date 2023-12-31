5499bc220edb4b0d10ababed170c9b9002080f94
abmarrero <a.ernestomarrerol@gmail.com>
2023-12-31 10:55:21 -0500

    login endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,38 @@
-const { response } = require("express")
+const { response } = require("express");
+const Usuario = require("../models/usuario");
+const bcryptjs = require('bcryptjs');
 
-
-const login = (req, res = response) => {
+const login = async(req, res = response) => {
     
-    res.json({
-        msg: 'login ok'
-    })
+    const { correo, password} = req.body;
+    try {
+        const usuario = await Usuario.findOne({correo})
+        if(!usuario){
+           return res.status(400).json({
+                mes: 'Usuario / contraseña no son correctos - correo'
+            });
+        }
+        if(!usuario.estado){
+           return res.status(400).json({
+                mes: 'Usuario / contraseña no son correctos - estado:false'
+            });
+        }
+        const validPassword = bcryptjs.compareSync(password,usuario.password)
+        if(!validPassword){
+            return res.status(400).json({
+                 mes: 'Usuario / contraseña no son correctos - contraseña'
+             });
+         }
+
+        res.json({
+            msg: 'login ok'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            mes: 'hable con el administrador'
+        });    
+    }
   }
 
 
